94c1e336317c29aa576e36dba66831a139609642
jrussellrivers <66189150+jrussellrivers@users.noreply.github.com>
2024-12-30 01:14:02 -0500

    Testing solo box - wip

---
+++ npc_ship_test.gd
@@ -0,0 +1,12 @@
+extends MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	pass
+	#self.global_position.z -= 0.1
